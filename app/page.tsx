80659a469b8c13df75ff21d48114b7a446cada3e
okyokuto <iherewithmyheart@gmail.com>
2023-12-27 15:38:21 +0800

    chore: init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className='grid h-screen w-screen place-items-center'>
+      <h1 className='scroll-m-20 font-[Georgia] text-4xl font-extrabold italic tracking-tight lg:text-5xl'>
+        wkk.im
+      </h1>
+    </main>
+  )
+}
