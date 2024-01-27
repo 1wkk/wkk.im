477e55b685e00c225d36d92c7675c20e218dd3dd
okyokuto <iherewithmyheart@gmail.com>
2024-01-27 22:39:25 +0800

    feat: redirect wkk.im to notion page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://wkk1.notion.site/wkk-185f530dac7443e3957e827a106c4b06?pvs=4',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
